a70a10ac3c276b46745352226a2023d588dd9ea9
michaelchen <jr.michael.chen@gmail.com>
2018-11-20 16:17:32 -0500

    Initial Commit

---
+++ FLL_2018_Scorer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FLL_2018_Scorer
+//
+//  Created by Michael Chen on 11/20/18.
+//  Copyright © 2018 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
