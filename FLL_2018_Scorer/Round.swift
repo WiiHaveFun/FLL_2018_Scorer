a7b9c2e308a03c6c3d01f7a8e1eb4b7484af88ca
michaelchen <jr.michael.chen@gmail.com>
2018-11-21 15:38:38 -0500

    Created the first text UI and segmented control UI for the first mission. Also, updated the round.swift file

--- FLL_2018_Scorer/Round.swift
+++ FLL_2018_Scorer/Round.swift
@@ -1,8 +1,8 @@
 //
 //  Round.swift
-//  FLL_2018_Scorer
+//  FLL 2018 Scorer
 //
-//  Created by Michael Chen on 11/20/18.
+//  Created by Michael Chen on 10/7/18.
 //  Copyright © 2018 Michael Chen. All rights reserved.
 //
 
@@ -12,6 +12,180 @@ struct Round {
     
     let constants = Constants()
     
+    private var totalScore = 0
+    
+    enum Position {
+        case No
+        case Partially
+        case Completely
+    }
+    
+    //M01 - Space Travel
+    var isM01_01Done = false {
+        didSet {
+            if isM01_01Done == true {
+                M01_01Score = constants.M01_01Score
+            } else {
+                M01_01Score = 0
+            }
+        }
+    }
+    var isM01_02Done = false {
+        didSet {
+            if isM01_02Done == true {
+                M01_02Score = constants.M01_02Score
+            } else {
+                M01_02Score = 0
+            }
+        }
+    }
+    var isM01_03Done = false {
+        didSet {
+            if isM01_03Done == true {
+                M01_03Score = constants.M01_03Score
+            } else {
+                M01_03Score = 0
+            }
+        }
+    }
+    var M01_01Score = 0
+    var M01_02Score = 0
+    var M01_03Score = 0
+    
+    
+    
+    //M02 - Solar Panel Array
+    var isM02_01Done = false {
+        didSet {
+            if isM02_01Done == true {
+                M02_01Score = constants.M02_01Score
+            } else {
+                M02_01Score = 0
+            }
+        }
+    }
+    var isM02_02Done = false {
+        didSet {
+            if isM02_02Done == true {
+                M02_02Score = constants.M02_02Score
+            } else {
+                M02_02Score = 0
+            }
+        }
+    }
+    var M02_01Score = 0
+    var M02_02Score = 0
+    
+    //M03 - 3D Printing
+    var isM03_01Done = false {
+        didSet {
+            if isM03_01Done == true {
+                M03_01Score = constants.M03_01Score
+                isM03_02Done = false
+            } else {
+                M03_01Score = 0
+            }
+        }
+    }
+    var isM03_02Done = false {
+        didSet {
+            if isM03_02Done == true {
+                M03_02Score = constants.M03_02Score
+                isM03_01Done = false
+            } else {
+                M03_02Score = 0
+            }
+        }
+    }
+    var M03_01Score = 0
+    var M03_02Score = 0
+    
+    //M04 - Crater Crossing
+    var isM04_01Done = false
+    var M04_01Score = 0
+    
+    //M05 - Extraction
+    var isM05_01Done = false
+    var isM05_02Done = false
+    var isM05_03Done = false
+    var isM05_04Done = false
+    var M05_01Score = 0
+    var M05_02Score = 0
+    var M05_03Score = 0
+    var M05_04Score = 0
+    
+    //M06 - Space Station Modules: 0
+    var M06_01Score = 0
+    var M06_02Score = 0
+    var M06_03Score = 0
+    
+    //M07 - Space Walk Emergency
+    var M07_01Status = Position.No
+    var M07_01Score = 0
+    
+    //M08 - Aerobic Exercise
+    var isM08_01Done = false
+    var isM08_02Done = false
+    var isM08_03Done = false
+    var M08_01Score = 0
+    var M08_02Score = 0
+    var M08_03Score = 0
+    
+    //M09 - Strength Exercise
+    var isM09_01Done = false
+    var M09_01Score = 0
+    
+    //M10 - Food Production
+    var isM10_01Done = false
+    var M10_01Score = 0
+    
+    //M11 - Escape Velocity
+    var isM11_01Done = false
+    var M11_01Score = 0
+    
+    //M12 - Satellite Orbits
+    var M12_01NumberOfSatellites = 0
+    var M12_01Score = 0
+    
+    //M13 - Observatory
+    var isM13_01Done = false
+    var isM13_02Done = false
+    var isM13_03Done = false
+    var M13_01Score = 0
+    var M13_02Score = 0
+    var M13_03Score = 0
+    
+    //M14 - Meteoroid Deflection
+    var M14MeteoroidsInCenter = 0
+    var M14MeteoroidsInSide = 0
+    var M14MeteoroidsInCenterScore = 0
+    var M14MeteoroidsInSideScore = 0
+    
+    //M15 - Lander Touch-Down
+    var isM15_01Done = false
+    var isM15_02Done = false
+    var isM15_03Done = false
+    var M15_01Score = 0
+    var M15_02Score = 0
+    var M15_03Score = 0
+    
+    //P01 - Penalty
+    let deductionsPerPenalty = -3
+    var numberOfPenalties = 0
+    var penaltiesScore = 0
+    
+    func calculatePenalties(by penalties: Int) -> Int {
+        return deductionsPerPenalty * penalties
+    }
+    
+    mutating func calculateTotalScore() {
+        totalScore = M01_01Score + M01_02Score + M01_03Score + M02_01Score + M02_02Score + M03_01Score + M03_02Score + M04_01Score + M05_01Score + M05_02Score + M05_03Score + M05_04Score + M06_01Score + M06_02Score + M06_03Score + M07_01Score + M08_01Score + M08_02Score + M08_03Score + M09_01Score + M10_01Score + M11_01Score + M12_01Score + M13_01Score + M13_02Score + M13_03Score + M14MeteoroidsInSideScore + M14MeteoroidsInCenterScore + M15_01Score + M15_02Score + M15_03Score + calculatePenalties(by: numberOfPenalties)
+    }
+    
+    mutating func getTotalScore() -> Int {
+        calculateTotalScore()
+        return totalScore
+    }
 }
 
 struct Constants {
@@ -25,3 +199,4 @@ struct Constants {
     let M03_01Score = 22
     let M03_02Score = 18
 }
+
--- FLL_2018_Scorer/ScoringViewController.swift
+++ FLL_2018_Scorer/ScoringViewController.swift
@@ -14,16 +14,102 @@ class ScoringViewController: UIViewController {
     
     //Constants for UILabels
     let labelFontSize = 30.0
+    let labelX = 10.0
+    let labelWidth = 748.0
+    let labelHeight = 40.0
+    let textHeight = 70.0
     
-    //Initializing Mission UI
+    //Outlet to scroll view
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //Outlet to total score label
+    @IBOutlet weak var totalScore: UILabel!
+    
+    /*Initializing Mission UI*/
     
     //M01 Space Travel
-    let mi01Title = UILabel(frame: CGRect(x: 10.0, y: 0, width: 748.0, height: 36.0))
+    var mi01Title: UILabel!
+    var mi01_01Text: UITextView!
+    var mi01_01Seg: UISegmentedControl!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.contentSize = view.frame.size
         // Do any additional setup after loading the view, typically from a nib.
+        initM01()
+    }
+    
+    /**
+        Initializes the first mission's UI
+    */
+    func initM01() {
+        //Inits UI
+        mi01Title = UILabel(frame: CGRect(x: labelX, y: 0.0, width: labelWidth, height: labelHeight))
+        mi01Title.text = "M01 - Space Travel"
+        mi01Title.font = mi01Title.font.withSize(CGFloat(labelFontSize))
+        scrollView.addSubview(mi01Title)
+        //Constraints
+        let leadingTitleConstraint = NSLayoutConstraint(item: mi01Title, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 10.0)
+        let trailingTitleConstraint = NSLayoutConstraint(item: mi01Title, attribute: .trailing, relatedBy: .lessThanOrEqual, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 10.0)
+        let topTitleConstraint = NSLayoutConstraint(item: mi01Title, attribute: .top, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .top, multiplier: 1.0, constant: 0.0)
+        view.addConstraints([leadingTitleConstraint, trailingTitleConstraint, topTitleConstraint])
+        
+        mi01_01Text = UITextView(frame: CGRect(x: labelX, y: 50.0, width: labelWidth, height: textHeight))
+        mi01_01Text.text = "Vehicle Payload rolled past first track connection"
+        mi01_01Text.font = UIFont.systemFont(ofSize: 20.0)
+        mi01_01Text.isScrollEnabled = false
+        mi01_01Text.isEditable = false
+        mi01_01Text.isSelectable = false
+        scrollView.addSubview(mi01_01Text)
+        let leadingTextConstraint01 = NSLayoutConstraint(item: mi01_01Text, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 10.0)
+        let trailingTextConstraint01 = NSLayoutConstraint(item: mi01_01Text, attribute: .trailing, relatedBy: .lessThanOrEqual, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 10.0)
+        let topTextConstraint01 = NSLayoutConstraint(item: mi01_01Text, attribute: .top, relatedBy: .equal, toItem: mi01Title, attribute: .bottom, multiplier: 1.0, constant: 10.0)
+        view.addConstraints([leadingTextConstraint01, trailingTextConstraint01, topTextConstraint01])
+        
+        mi01_01Seg = UISegmentedControl(items: ["No", "Yes"])
+        mi01_01Seg.frame = CGRect(x: 244, y: 130.0, width: 280, height: 30)
+        mi01_01Seg.selectedSegmentIndex = 0
+        mi01_01Seg.addTarget(self, action: #selector(mi01SegAction), for: .valueChanged)
+        scrollView.addSubview(mi01_01Seg)
+        let leadingSegConstraint01 = NSLayoutConstraint(item: mi01_01Seg, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 244.0)
+        let trailingSegConstraint01 = NSLayoutConstraint(item: mi01_01Seg, attribute: .trailing, relatedBy: .lessThanOrEqual, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 244.0)
+        let topSegConstraint01 = NSLayoutConstraint(item: mi01_01Seg, attribute: .top, relatedBy: .equal, toItem: mi01_01Text, attribute: .bottom, multiplier: 1.0, constant: 10.0)
+        view.addConstraints([leadingSegConstraint01, trailingSegConstraint01, topSegConstraint01])
+        //Adds seg control to outlet
+        mi01SegControls.append(mi01_01Seg)
+        
+    }
+    
+    //Mission 01 Outlet
+    var mi01SegControls: [UISegmentedControl] = []
+    
+    //Mission 01 Action
+    @objc func mi01SegAction(sender: UISegmentedControl) {
+        if let indexOfControl = mi01SegControls.index(of: sender) {
+            if (indexOfControl == 0) {
+                if (sender.selectedSegmentIndex == 1) {
+                    round.isM01_01Done = true
+                } else {
+                    round.isM01_01Done = false
+                }
+            } else if (indexOfControl == 1) {
+                if (sender.selectedSegmentIndex == 1) {
+                    round.isM01_02Done = true
+                } else {
+                    round.isM01_02Done = false
+                }
+            } else if (indexOfControl == 2) {
+                if (sender.selectedSegmentIndex == 1) {
+                    round.isM01_03Done = true
+                } else {
+                    round.isM01_03Done = false
+                }
+            }
+            
+            // TODO: Get the local mission scorer finished
+            //mi01Score.text = "Score: \(round.M01_01Score + round.M01_02Score round.M01_03Score)"
+            totalScore.text = "Score: \(round.getTotalScore())"
+        }
     }
 
 
